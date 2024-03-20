18af03420cfc3d0443ac505ebfdf8189bec50bbb
farjanaIslamNipa <nipa.farzana87@gmail.com>
2024-03-20 15:48:49 +0600

    updated the base api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5000/api/v1'}),
+  baseQuery: fetchBaseQuery({baseUrl: 'https://health-aid-hub-backend.vercel.app/api/v1'}),
   tagTypes: ['supplies'],
   endpoints: () => ({})
 })
